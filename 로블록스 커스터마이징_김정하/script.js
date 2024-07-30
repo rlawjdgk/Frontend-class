ea540c3f9fa3246b655e54adb169895e09f0ece6
rlawjdgk <gloria0205p0@naver.com>
2024-07-30 09:22:58 +0900

    roblox_kimjeongha

---
+++ 로블록스 커스터마이징_김정하/script.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector("#open");
+const modalBox = document.querySelector(".container");
+const close = document.querySelector(".fa-x");
+const firstDiv = document.querySelector("#wrapper > div:first-child");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalBox.classList.add("active");
+  firstDiv.classList.add("active");
+});
+close.addEventListener("click", () => {
+  modalBox.classList.remove("active");
+  firstDiv.classList.remove("active");
+});
