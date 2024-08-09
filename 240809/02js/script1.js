786d957d50f02b8087f38a3f5e9d6b91a3417362
rlawjdgk <gloria0205p0@naver.com>
2024-08-09 18:56:38 +0900

    240809

---
+++ 240809/02js/script1.js
@@ -0,0 +1,42 @@
+// // Math 수학객체 = 함수
+
+// let num = 2.1234;
+// let num01 = 2.8765;
+
+// let maxNum = Math.max(10, 5, 8, 30);
+// let minNum = Math.min(10, 5, 8, 30);
+// // 대체불가 (이 기능에서 가장 좋음)
+// let roundNum = Math.round(num);
+// let floorNum = Math.floor(num01);
+// let ceilNum = Math.ceil(num);
+// let rndNum = Math.random();
+// let piNum = Math.PI;
+
+// document.write(maxNum, "<br />");
+// document.write(minNum, "<br />");
+// document.write(roundNum, "<br />");
+// document.write(floorNum, "<br />");
+// document.write(ceilNum, "<br />");
+// document.write(rndNum, "<br />");
+// document.write(piNum, "<br />");
+
+const character = document.querySelector(".character");
+// console.log(character);
+
+let degree = 0;
+
+const loop = () => {
+  const rotation = (degree * Math.PI) / 180;
+  // console.log(rotation);
+  const targetX = window.innerWidth / 2 - 50 + 100 * Math.cos(rotation);
+  const targetY = window.innerHeight / 2 - 50 + 100 * Math.sin(rotation);
+
+  character.style.left = `${targetX}px`;
+  character.style.top = `${targetY}px`;
+
+  degree += 1;
+
+  requestAnimationFrame(loop);
+};
+
+loop();
