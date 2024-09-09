ac078bf0aa9b42b1a187050f0f06378345db7738
rlawjdgk <gloria0205p0@naver.com>
2024-09-09 09:24:50 +0900

    240909

---
+++ 240905_03reactproject/src/component/TodoList.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./TodoList.css";
+import TodoItem from "./TodoItem";
+
+const TodoList = ({ todo }) => {
+  const [search, setSearch] = useState("");
+  const onchangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearchResult = () => {
+    return search === ""
+      ? todo
+      : todo.filter((it) =>
+          it.content.toLowerCase().includes(search.toLowerCase())
+        );
+  };
+  return (
+    <div className="TodoList">
+      <h4>Todo List 🔍</h4>
+      <input
+        value={search}
+        onChange={onchangeSearch}
+        className="searchbar"
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="list_wrapper">
+        {getSearchResult().map((it) => (
+          <TodoItem key={it.id} {...it} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
