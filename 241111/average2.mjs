7086b7675e9ae6709463bcd8be5874f56e82e9c4
rlawjdgk <gloria0205p0@naver.com>
2024-11-11 18:38:29 +0900

    241111

---
+++ 241111/average2.mjs
@@ -0,0 +1,14 @@
+// 배열이라는 자료구조를 활용해서 3개의 숫자의 평균값을 구함
+
+// const arr = [87, 70, 100];
+const arr = [87, 70, 100, 55];
+
+let average = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  average += arr[i];
+}
+
+average /= arr.length;
+
+// 값이 추가될때마다 1번의 수정 필요
