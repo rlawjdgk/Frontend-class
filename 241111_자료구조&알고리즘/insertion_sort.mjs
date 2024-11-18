a36e7eeb65ce17bd11b9f43d6bf013ac5954e095
rlawjdgk <gloria0205p0@naver.com>
2024-11-18 16:56:11 +0900

    241118

---
+++ 241111_자료구조&알고리즘/insertion_sort.mjs
@@ -0,0 +1,27 @@
+// 삽입 정렬
+// let arr = [1, 2, 3, 4, 5, 6];
+
+let arr = [4, 1, 5, 3, 6, 2];
+
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let insertingData = arr[i];
+    let j;
+    for (j = i - 1; j >= 0; j--) {
+      if (arr[j] > insertingData) {
+        arr[j + 1] = arr[j];
+      } else {
+        break;
+      }
+    }
+    arr[j + 1] = insertingData;
+  }
+};
+
+console.log("==== 정렬 전 ====");
+console.log(arr);
+
+insertionSort(arr);
+
+console.log("==== 정렬 후 ====");
+console.log(arr);
