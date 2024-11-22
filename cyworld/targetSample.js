be928d1313c806df05d614ece695bc7ba3055fd3
rlawjdgk <gloria0205p0@naver.com>
2024-11-22 18:01:38 +0900

    241122

--- cyworld/targetSample.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector("button");
-
-const buttonAction = (e) => {
-  console.log(e);
-  console.log(e.target);
-  console.log(e.currentTarget);
-};
-
-button.addEventListener("click", buttonAction);
