28b89f733ad4d7a161aaa522042ba34dcd47a7c1
rlawjdgk <gloria0205p0@naver.com>
2024-12-09 14:32:21 +0900

    241209

--- 241120_nodejs/src/controllers/userController.js
+++ 241120_nodejs/src/controllers/userController.js
@@ -231,7 +231,7 @@ export const postChangePassword = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id);
+  const user = await User.findById(id).populate("video");
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not Found." });
   }
